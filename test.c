7807ee0d54c013ed02835a5646480329889fef60
kimhannaya <csato31212@naver.com>
2025-10-17 12:54:16 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <string.h> //문자열을 다룰 수 있는 헤더파일 포함
 
 int main(void){
-    printf("asdasdasd");
+    /*
+    char fruit[20] = "strawberry"; //char 배열 선언과 문자열 초기화
+    printf("딸기 : %s\n", fruit); //배열명으로 저장된 문자열 출력
+    printf("딸기쨈 : %s %s\n", fruit, "jam"); //문자열 상수를 직접 %s로 출력
 
+
+    printf("%s\n", fruit); //strawberry 출력
+    strcpy(fruit, "banana"); //fruit에 banana 복사
+    printf("%s\n", fruit); //banana 출력
+    
+    char grade;
+    char name[20];
+    printf("학점 입력 : "); 
+    scanf("%c", &grade); //grade 변수에 학점 문자 입력
+    printf("이름 입력 : "); 
+    scanf("%s", name); //name 배열에 이름 문자열 입력, &을 사용하지 않음
+    printf("%s의 학점은 %c입니다\n", name, grade);
+
+    int a = 10, b = 10;
+    ++a; // 변수의 값을 1만큼 증가
+    --b; // 변수의 값을 1만큼 감소
+    printf("a : %d\n", a);
+    printf("a : %d\n", a);
+    printf("b : %d\n", b); 
+
+    int a = 10;
+    double b = 3.4;
+    printf("int형 변수의 크기 : %d\n", sizeof(a));
+    printf("double형 변수의 크기 : %d\n", sizeof(b));
+    printf("정수형 상수의 크기 : %d\n", sizeof(10));
+    printf("수식의 결과값의 크기 : %d\n", sizeof(1.5 + 3.4));
+    printf("char의 자료형의 크기 : %d\n", sizeof(char)); 
+    */
+
+   
     return 0;
 }
